dab735bdcdff379e097cbeeab957512b157247da
MattM <mattm4300@gmail.com>
2017-02-16 23:34:43 -0600

    Adding GUI import

--- test.py
+++ test.py
@@ -9,43 +9,10 @@ import os
 
 from configloader import loadConfigs
 import logging
-from tkinter import *
-from tkinter.ttk import *
+import GUI
 
 # TODO: Add command line arguments.  These will override written file
 #       instructions eventually.
-
-class Application(Frame):
-	def __init__(self, master):
-		Frame.__init__(self, master)
-		self.grid()
-		self.createWidgets()
-		self.master = master
-
-	def createWidgets(self):
-		self.menubar = Menu(self.master)
-
-		self.filemenu = Menu(self.menubar, tearoff=0)
-		self.filemenu.add_command(label="Open", command=self.say_hi)
-		self.filemenu.add_command(label="Save", command=self.say_hi)
-		self.filemenu.add_separator()
-		self.filemenu.add_command(label="Exit", command=self.master.quit)
-		self.menubar.add_cascade(label = "File", menu = self.filemenu)
-
-		self.master.config(menu = self.menubar)
-
-		self.lab = Label(self, text = "My first label").grid(row = 1)
-		self.e1 = Entry(self)
-		self.v = StringVar()
-		self.e1["textvariable"] = self.v
-		self.e1.bind("<Key-Return>", self.printstuff)
-		self.e1.grid(row = 2)
-
-	def say_hi(self):
-		print("hi")
-
-	def printstuff(self, event):
-		print(self.v.get())
 
 
 def setup():
@@ -65,9 +32,7 @@ def main():
 	setup()
 	logging.info("Program started.")
 	logging.debug("Starting GUI.")
-	root = Tk()
-	app = Application(master = root)
-	app.mainloop()
+	GUI.GUI.mainloop()
 	logging.debug("GUI exited.")
 	logging.info("Program exiting.")
 
